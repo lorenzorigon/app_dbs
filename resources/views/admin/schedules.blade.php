2b25ed117bb5329bb22dc97a433e0730b511e422
Lorenzo <lorenzocardoso@hotmail.com>
2021-10-06 10:58:38 -0300

    adicionando tela admin para visualiar schedules

---
+++ resources/views/admin/schedules.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Agenda de Hoje 
+        </div>
+        <div class="card-body">
+            <table class="table">
+                1-
+                2-
+                3-
+            </table>
+        </div>
+    </div>
+@endsection
