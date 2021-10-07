c8a639caf9e5c0b2559d098647c4424a01f273d3
Lorenzo <lorenzocardoso@hotmail.com>
2021-10-07 09:47:01 -0300

    a

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -16,39 +16,38 @@
 
                     <div class="container">
                         <div class="row text-center">
-                            <form action="">
-                                <!-- Seleção de serviço -->
-                                <div class="row align-items-center mb-2 ml-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="service" id="corte">
-                                        <label class="form-check-label mr-2" for="corte"> Corte </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="service" id="barba" checked>
-                                        <label class="form-check-label" for="barba"> Barba </label>
-                                    </div>
-                                </div>
                             
-                                <!-- Campo de data -->
-                                <div class="col-12 mb-2">
-                                    <input type="date" class="form-control" id="date">
+                            <!-- Seleção de serviço -->
+                            <div class="row align-items-center mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="service" id="corte">
+                                    <label class="form-check-label mr-2" for="corte"> Corte </label>
                                 </div>
-
-                                <!-- Botões de horário-->
-                                @for ( $i = 13; $i < 20; $i++)
-                                    <div class="col-6">
-                                        <button class="btn btn-primary btn-lg mb-1">{{$i}}:00</button>
-                                    </div>
-                                    <div class="col-6">
-                                        <button class="btn btn-primary btn-lg mb-1">{{$i}}:30</button>
-                                    </div>
-                                @endfor
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="service" id="barba" checked>
+                                    <label class="form-check-label" for="barba"> Barba </label>
+                                </div>
+                            </div>
                         
-                                <!-- Botão para agendar horário -->
-                                <div class="col-12">
-                                    <button class="btn btn-success mt-1" style="width:100%;font-size:20px">Agendar</button>
+                            <!-- Campo de data -->
+                            <div class="col-12 mb-2">
+                                <input type="date" class="form-control" id="date">
+                            </div>
+
+                            <!-- Botões de horário-->
+                            @for ( $i = 13; $i < 20; $i++)
+                                <div class="col-6">
+                                    <button class="btn btn-primary btn-lg mb-1">{{$i}}:00</button>
                                 </div>
-                            </form>
+                                <div class="col-6">
+                                    <button class="btn btn-primary btn-lg mb-1">{{$i}}:30</button>
+                                </div>
+                            @endfor
+                            
+                            <!-- Botão para agendar horário -->
+                            <div class="col-12">
+                                <button class="btn btn-success mt-1" style="width:100%;font-size:20px">Agendar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
