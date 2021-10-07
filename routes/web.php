6dfcb9718c13bd09563e390387557168f34580af
Lorenzo <lorenzocardoso@hotmail.com>
2021-10-07 08:07:14 -0300

    ajustando pra aplicação começar na tela de login

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
 
 //route resource para schedule
 Route::resource('schedule', App\Http\Controllers\ScheduleController::class);
